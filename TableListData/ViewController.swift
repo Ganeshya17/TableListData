1c15a83c988a365e8bc393378b6c733cf39ca06b
Ganesh Raja <ganeshya17@gmail.com>
2020-07-10 18:04:13 +0530

    Initial Commit

---
+++ TableListData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableListData
+//
+//  Created by Syed Riyaz on 10/07/20.
+//  Copyright © 2020 Ganesh Raja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
